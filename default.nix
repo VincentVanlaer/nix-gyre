c861586611ff7a50d65cd5a65d86a6679d2ba24e
Vincent Vanlaer <vincent.vanlaer@kuleuven.be>
2024-03-14 09:23:35 +0100

    Don't use overlay for nixpkgs

--- default.nix
+++ default.nix
@@ -1,11 +1,8 @@
-with import <nixpkgs> {
-  overlays = [
-    (self: super: {hdf5 = super.hdf5.overrideAttrs (finalAttrs: prevAttrs: {patches = (prevAttrs.patches or []) ++ [./hdf5.patch];});})
-  ];
-}; {
+with import <nixpkgs> {}; {
   gyre = callPackage ./gyre.nix {
     lapack95 = callPackage ./lapack95.nix {};
     odepack = callPackage ./odepack.nix {};
+    hdf5-fortran = hdf5-fortran.overrideAttrs (finalAttrs: prevAttrs: {patches = (prevAttrs.patches or []) ++ [./hdf5.patch];});
     python3 = python3.override {
       packageOverrides = self: super: {fypp = callPackage ./fypp.nix {buildPythonPackage = super.buildPythonPackage;};};
     };
