a30fa1d0a0bd33d85a40fb1fb18082bfb77c5e7d
Vincent Vanlaer <vincent.vanlaer@kuleuven.be>
2024-03-22 20:01:13 +0000

    Refactor default.nix

--- default.nix
+++ default.nix
@@ -1,16 +1,24 @@
-{ pkgs ? import <nixpkgs> {}}: with pkgs; {
+{ pkgs ? import <nixpkgs> {}}: let
+  callPackage = pkgs.callPackage;
+  hdf5-fortran = pkgs.hdf5-fortran;
+  python3 = pkgs.python3;
+in rec {
+  lapack95 = callPackage ./lapack95.nix {};
+  odepack = callPackage ./odepack.nix {};
+  hdf5 = hdf5-fortran.overrideAttrs (finalAttrs: prevAttrs: {patches = (prevAttrs.patches or []) ++ [./hdf5.patch];});
+  python3-with-fypp = python3.override {
+    packageOverrides = self: super: {fypp = callPackage ./fypp.nix {buildPythonPackage = super.buildPythonPackage;};};
+  };
+  fpx3 = callPackage ./fpx3.nix {};
+  fpx3_deps = callPackage ./fpx3_deps.nix {};
+
   gyre-next = callPackage ./gyre-next.nix {
-    lapack95 = callPackage ./lapack95.nix {};
-    odepack = callPackage ./odepack.nix {};
-    hdf5-fortran = hdf5-fortran.overrideAttrs (finalAttrs: prevAttrs: {patches = (prevAttrs.patches or []) ++ [./hdf5.patch];});
-    python3 = python3.override {
-      packageOverrides = self: super: {fypp = callPackage ./fypp.nix {buildPythonPackage = super.buildPythonPackage;};};
-    };
+    inherit lapack95 odepack;
+    hdf5-fortran = hdf5;
+    python3 = python3-with-fypp;
   };
   gyre-71 = callPackage ./gyre.nix {
-    lapack95 = callPackage ./lapack95.nix {};
-    hdf5-fortran = hdf5-fortran.overrideAttrs (finalAttrs: prevAttrs: {patches = (prevAttrs.patches or []) ++ [./hdf5.patch];});
-    fpx3 = callPackage ./fpx3.nix {};
-    fpx3_deps = callPackage ./fpx3_deps.nix {};
+    inherit lapack95 fpx3 fpx3_deps;
+    hdf5-fortran = hdf5;
   };
 }
